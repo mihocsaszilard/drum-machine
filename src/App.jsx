731da196c45349b3f5d0fe5c355b6f34805eca28
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-23 21:13:53 +0200

    added song set changer

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ import { set1, set2 } from "./components/songs";
 class App extends Component {
   state = {
     toPlay: set1,
+    setName: "Push to change Song Pack ",
   };
 
   changeSongSet = () => {
     if (this.state.toPlay === set1) {
       this.setState({
         toPlay: set2,
+        setName: "Song Pack 2",
       });
     } else {
       this.setState({
         toPlay: set1,
+        setName: "Song Pack 1",
       });
     }
   };
@@ -24,10 +27,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="drum-machine">
-          <Display />
-          <Pads set={this.state.toPlay} />
-          <button onClick={this.changeSongSet}>Change Sounds</button>
+        <div className="wrapper">
+          <div id="drum-machine">
+            <Display />
+            <Pads set={this.state.toPlay} />
+          </div>{" "}
+          <div className="wrapper">
+            <p id="song-set">{this.state.setName}</p>
+            <button id="change-set" onClick={this.changeSongSet}>
+              Change Songs
+            </button>
+          </div>
         </div>
       </div>
     );
